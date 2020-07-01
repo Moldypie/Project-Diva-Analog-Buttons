dc0c3d9379a8c43e9bc4a670940279c8e03d897c
Moldypie <48046412+Moldypie@users.noreply.github.com>
2020-07-01 16:29:23 -0500

    Update diva.ino

--- diva.ino
+++ diva.ino
@@ -1,22 +1,42 @@
-#include <Joystick.h>
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD, 4, 0,
- false, false, false, false, false, false, false, false, false, false, false);
-
-byte ButtonPins[] = {A0,A1,A2,A3};
-byte ButtonCount = sizeof(ButtonPins) / sizeof(ButtonPins[0]);
+#include <Keyboard.h>
+byte ButtonPins[] = {A0,A1,A2,A3,A4};
+byte SinglePins[] = {A5,A6,A7,A8};
 
 void setup() {
+  for(int i=0;i<5;i++){
+    pinMode(ButtonPins[i], INPUT_PULLUP);
+  }
   Serial.begin(9600) ;
-  Joystick.begin(false);
+  Keyboard.begin();
 }
 
 void loop() {
-  for(int i=0;i<ButtonCount;i++) {
-    if(analogRead(ButtonPins[i]) > 0){
-      Joystick.setButton (ButtonPins[i],1);
-    }else{
-      Joystick.setButton (ButtonPins[i],0);
-    }
+  if(digitalRead(ButtonPins[0]) == 0){
+    Keyboard.press(0x27);
+  }else{
+    Keyboard.release(0x27);
   }
-  Joystick.sendState();
+  if(digitalRead(ButtonPins[1]) == 0){
+    Keyboard.press(0x28);
+  }else{
+    Keyboard.release(0x28);
+  }
+  if(digitalRead(ButtonPins[2]) == 0){
+    Keyboard.press(0x25);
+  }else{
+    Keyboard.release(0x25);
+  }
+  if(digitalRead(ButtonPins[3]) == 0){
+    Keyboard.press(0x26);
+  }else{
+    Keyboard.release(0x26);
+  }
+  if(digitalRead(ButtonPins[4]) == 1){
+    Keyboard.press(0x29);
+  }else{
+    Keyboard.release(0x29);
+  }
+  for(int i=0;i<4;i++) {
+    digitalWrite(SinglePins[i], digitalRead(ButtonPins[i]));
+  }
 }
